caa816d8da80d5ad55c8461f47a22b28e456230f
John Chavanne <jchavanne@paloaltonetworks.com>
2022-06-10 11:40:42 -0400

    add improvements

--- aws/iam/ec2-launch-with-ami-tags.tf
+++ aws/iam/ec2-launch-with-ami-tags.tf
@@ -1,5 +1,30 @@
 # Ref: https://aws.amazon.com/premiumsupport/knowledge-center/restrict-launch-tagged-ami/
 
+# Variables
+# Placed here only for easy reference
+
+# Ensure to enter YOUR ACCOUNT ID & REGION
+variable "account_id" {
+  type        = string
+  description = "AWS Account ID"
+  default     = "111111111111"
+}
+variable "region" {
+  type        = string
+  description = "AWS region"
+  default     = "us-east-1"
+}
+variable "ami_tag_key" {
+  type        = string
+  description = "Custom AMI tag key"
+  default     = "image"
+}
+variable "ami_tag_value" {
+  type        = string
+  description = "Custom AMI tag value"
+  default     = "defender"
+}
+
 # Policy
 resource "aws_iam_policy" "ec2-launch-policy" {
   name        = "EC2LaunchwithAMIsAndTags-Policy"
@@ -9,41 +34,42 @@ resource "aws_iam_policy" "ec2-launch-policy" {
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    Version: "2012-10-17",
+    Statement: [
       {
-        "Sid": "ReadOnlyAccess",
-        "Effect": "Allow",
-        "Action": [
+        Sid: "EC2Access",
+        Effect: "Allow",
+        Action: [
           "ec2:Describe*",
           "ec2:GetConsole*",
-          "cloudwatch:DescribeAlarms",
-          "cloudwatch:GetMetricStatistics",
-          "iam:ListInstanceProfiles"
+          "ec2:CreateKeyPair",
+          "ec2:AssociateIamInstanceProfile",
+          "iam:ListInstanceProfiles",
+          "iam:PassRole"
         ],
-        "Resource": "*"
+        Resource: "*"
       },
       {
-        "Sid": "ActionsRequiredtoRunInstancesInVPC",
-        "Effect": "Allow",
-        "Action": "ec2:RunInstances",
-        "Resource": [
-          "arn:aws:ec2:us-east-1:AccountId:instance/*",
-          "arn:aws:ec2:us-east-1:AccountId:key-pair/*",
-          "arn:aws:ec2:us-east-1:AccountId:security-group/*",
-          "arn:aws:ec2:us-east-1:AccountId:volume/*",
-          "arn:aws:ec2:us-east-1:AccountId:network-interface/*",
-          "arn:aws:ec2:us-east-1:AccountId:subnet/*"
+        Sid: "ActionsRequiredtoRunInstancesInVPC",
+        Effect: "Allow",
+        Action: "ec2:RunInstances",
+        Resource: [
+          "arn:aws:ec2:${var.region}:${var.account_id}:instance/*",
+          "arn:aws:ec2:${var.region}:${var.account_id}:key-pair/*",
+          "arn:aws:ec2:${var.region}:${var.account_id}:security-group/*",
+          "arn:aws:ec2:${var.region}:${var.account_id}:volume/*",
+          "arn:aws:ec2:${var.region}:${var.account_id}:network-interface/*",
+          "arn:aws:ec2:${var.region}:${var.account_id}:subnet/*"
         ]
       },
       {
-        "Sid": "LaunchingEC2withAMIsAndTags",
-        "Effect": "Allow",
-        "Action": "ec2:RunInstances",
-        "Resource": "arn:aws:ec2:us-east-1::image/ami-*",
-        "Condition": {
-          "StringEquals": {
-            "ec2:ResourceTag/image": "defender"
+        Sid: "LaunchingEC2withAMIsAndTags",
+        Effect: "Allow",
+        Action: "ec2:RunInstances",
+        Resource: "arn:aws:ec2:${var.region}::image/ami-*",
+        Condition: {
+          StringEquals: {
+            "ec2:ResourceTag/${var.ami_tag_key}": "${var.ami_tag_value}"
           }
         } 
       }
@@ -55,15 +81,15 @@ resource "aws_iam_policy" "ec2-launch-policy" {
 resource "aws_iam_role" "ec2-access-role" {
   name                = "EC2LaunchwithAMIsAndTags-Role"
   assume_role_policy  = jsonencode({
-   "Version": "2012-10-17",
-   "Statement": [
+   Version: "2012-10-17",
+   Statement: [
      {
-       "Action": "sts:AssumeRole",
-       "Principal": {
-         "Service": "ec2.amazonaws.com"
+       Action: "sts:AssumeRole",
+       Principal: {
+         Service: "ec2.amazonaws.com"
        },
-       "Effect": "Allow",
-       "Sid": ""
+       Effect: "Allow",
+       Sid: ""
      }
    ]
   })
@@ -75,3 +101,35 @@ resource "aws_iam_policy_attachment" "ec2-policy-attachment" {
   roles      = ["${aws_iam_role.ec2-access-role.name}"]
   policy_arn = "${aws_iam_policy.ec2-launch-policy.arn}"
 }
+
+resource "aws_iam_user" "iam-user" {
+  name = "privesc3-ModifiedCreateEC2WithExistingInstanceProfile-user"
+  path = "/"
+}
+
+resource "aws_iam_access_key" "iam-user" {
+  user = aws_iam_user.iam-user.name
+}
+
+# This should migrate to a Group and Group policy attachment in future
+resource "aws_iam_user_policy_attachment" "user-attach-policy" {
+  user       = aws_iam_user.iam-user.name
+  policy_arn = aws_iam_policy.ec2-launch-policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "role-attach-policy" {
+  role       = aws_iam_role.ec2-access-role.name
+  policy_arn = aws_iam_policy.ec2-launch-policy.arn
+}
+
+# TODO - this needs testing
+#output "iam_access_key_id" {
+#  description = "The access key ID"
+#  value       = try(aws_iam_access_key.this[0].id, aws_iam_access_key.this_no_pgp[0].id, "")
+#}
+
+#output "iam_access_key_secret" {
+#  description = "The access key secret"
+#  value       = try(aws_iam_access_key.this_no_pgp[0].secret, "")
+#  sensitive   = true
+#}
